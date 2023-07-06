d7afa13bf2503d5109293664f5661eb525373352
Benjamin Kaspar <benjamin.kaspar@candylabs.de>
2023-07-06 11:30:43 +0200

    Update __init__.py

--- wagtail_opengraph_image_generator/__init__.py
+++ wagtail_opengraph_image_generator/__init__.py
@@ -1,14 +1,14 @@
 default_app_config = (
-    'wagtail_opengraph_image_generator.apps.WagtailOGImageGeneratorConfig'
+    "wagtail_opengraph_image_generator.apps.WagtailOGImageGeneratorConfig"
 )
 
-VERSION = (1, 1, 0, 'final')
+VERSION = (2, 0, 0, "final")
 
 
 def get_version():
-    version = '{}.{}'.format(VERSION[0], VERSION[1])
+    version = "{}.{}".format(VERSION[0], VERSION[1])
     if VERSION[2]:
-        version = '{}.{}'.format(version, VERSION[2])
-    if VERSION[3] != 'final':
-        version = '{}.{}'.format(version, VERSION[3])
+        version = "{}.{}".format(version, VERSION[2])
+    if VERSION[3] != "final":
+        version = "{}.{}".format(version, VERSION[3])
     return version
